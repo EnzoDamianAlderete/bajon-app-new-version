00444adba810360b838cb9eaf85b6dd6dd0a7d75
EnzoDamianAlderete <enzoalderete04@gmail.com>
2023-05-03 20:59:44 -0300

    test routes

---
+++ pages/carrito/carrito.tsx
@@ -0,0 +1,9 @@
+const carritoPage =()=>{
+    return(
+        <>
+        <h2>Carrito Page</h2>
+        </>
+    )
+}
+
+export default carritoPage;
---
+++ pages/carrito/index.tsx
@@ -0,0 +1,11 @@
+import carritoPage from "./carrito";
+
+const carritoContainer=()=>{
+    return(
+        <>
+        <h2>Carrito Page 22</h2>
+        </>
+    )
+}
+
+export default carritoContainer;
